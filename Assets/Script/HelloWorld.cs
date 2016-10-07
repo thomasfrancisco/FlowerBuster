a863cd4b32dd04ca467effaea2a35e646154b5ec
thomasfrancisco <thofrancisco@gmail.com>
2016-10-08 00:58:09 +0200

    First commit

---
+++ Assets/Script/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+    public string text;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(text);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
